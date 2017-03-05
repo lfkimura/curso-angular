c81b5816de8c656957c64cbb649dc45f0edc4c25
Luis Kimura <Luis Kimura>
2017-03-05 10:36:45 -0300

    diretiv minha foto

--- public/js/diretivas/minhas-diretivas.js
+++ public/js/diretivas/minhas-diretivas.js
@@ -1,6 +1,5 @@
 angular.module("minhasDiretivas",[])
 .directive("meuPainel", function(){
-	console.log("diretivas");
 	var ddo = {};
 	ddo.transclude=true;
 	ddo.restrict = 'AE';
@@ -8,6 +7,18 @@ angular.module("minhasDiretivas",[])
 		titulo: "@"
 	}
 	ddo.templateUrl = 'js/diretivas/meu-painel.html';
+	return ddo;
+
+})
+.directive("minhaFoto", function(){
+	var ddo = {};
+	ddo.transclude=false;
+	ddo.restrict = 'E';
+	ddo.scope = {
+		titulo: "@",
+		url: "@"
+	}
+	ddo.templateUrl = 'js/diretivas/minha-foto.html';
 	return ddo;
 
 });
